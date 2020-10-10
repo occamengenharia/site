bfac2123de8888abb772930ddd3d1bf9f81f9462
Gabriel <gprando55@gmail.com>
2020-10-09 23:28:24 -0300

    feat: add initial struct nextjs SSR and SSG :zap:

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+import { Container } from '../styles/pages/Home'
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <Head>
+        <title>Homepage</title>
+      </Head>
+
+      <h1>Next Starter structure</h1>
+    </Container>
+  )
+}
+
+export default Home
---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+const theme = {
+  colors: {
+    background: '#121214',
+    text: '#e1e1e6',
+    primary: '#0b50bb'
+  }
+}
+
+export default theme
