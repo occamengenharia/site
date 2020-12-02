40ea70db7255261a812a3e4822ab88168bd6499a
Daniel <danielfavero17@gmail.com>
2020-12-02 13:51:24 -0300

    chore: uptaded Home.ts styles and changed isometric image

--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -8,13 +8,17 @@ export const Container = styled.div`
   align-items: center;
   main {
     width: min(90vw, 500px);
+    display: flex;
+    justify-content: center;
+
     > section {
-      width: 100%;
+      max-width: 27.2rem;
       height: 100%;
       display: flex;
       flex-direction: column;
       justify-content: center;
-      align-items: center;
+      align-items: flex-start;
+
       h1 {
         font-size: 6.4rem;
         color: ${props => props.theme.colors.secundary};
@@ -22,18 +26,18 @@ export const Container = styled.div`
       h4 {
         font-size: 1.6rem;
         color: ${props => props.theme.colors.primary};
-        margin-top: 0.8rem;
+        margin-top: 0.2rem;
       }
 
       p {
-        margin-top: 0.8rem;
+        margin-top: 0.4rem;
         margin-bottom: 3.2rem;
         font-size: 1.2rem;
-        line-height: 32px;
         color: ${props => props.theme.colors.text};
       }
 
       > img {
+        align-self: center;
         width: 60%;
         margin-top: 6.4rem;
       }
@@ -41,13 +45,33 @@ export const Container = styled.div`
     > img {
       display: none;
     }
+
     @media (min-width: ${props => props.theme.breakpoints.md}px) {
-      width: min(90vw, 1100px);
+      width: min(90vw, 1000px);
       display: flex;
+      justify-content: space-between;
+
+      > section{
+        align-items: flex-start;
+        max-width: 41.6rem;
+
+        h1{
+           font-size: 9.6rem;
+        }
+
+        h4{
+          font-size: 2.4rem;
+        }
+
+        > img{
+          margin-top: 11.2rem;
+        }
+      }
 
       > img {
+        align-self: flex-start;
         display: initial;
-        max-height: 80rem;
+        max-width: 60rem;
       }
     }
   }
