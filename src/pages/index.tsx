1f0029b2a3b956d1cae34857e1119dd380bce174
Gabriel Prando <gprando55@gmail.com>
2020-12-08 23:28:48 -0300

    Revert "Home page with scroll snap and components"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,42 @@
-import * as Styles from '@/styles/pages/Home'
+import { Container } from '@/styles/pages/Home'
 import SEO from '@/components/SEO'
+
+import Link from '@/components/Link'
+import { FaInstagram, FaLinkedin, FaGithub } from 'react-icons/fa'
+import BtnTheme from '@/components/BtnTheme'
 
 const Home: React.FC = () => {
   const description = 'OCCAM Engenharia, Empresa Júnior de Computação UTFPR-PB'
   return (
-    <Styles.Page>
-      <Styles.Initial>Destaques</Styles.Initial>
-      <Styles.Actuation>Area de Atuacao</Styles.Actuation>
-      <Styles.Members>Mombros</Styles.Members>
-      <Styles.Footer>footer</Styles.Footer>
-    </Styles.Page>
+    <Container>
+      <SEO title="HOME" image="/banner.png" description={description} />
+      <main>
+        <section>
+          <h1>OOOPS...</h1>
+          <h4>Ainda estamos trabalhando nesse site</h4>
+          <p>Não deixe de acompanhar a gente nas redes sociais</p>
+          <BtnTheme />
+          <Link
+            icon={FaInstagram}
+            href="https://www.instagram.com/occamengenharia/"
+            text="Instagram"
+          />
+          <Link
+            icon={FaLinkedin}
+            href="https://www.linkedin.com/company/occamengenharia/"
+            text="Linkedin"
+          />
+          <Link
+            icon={FaGithub}
+            href="https://github.com/occamengenharia"
+            text="Github"
+          />
+
+          <img src="occam.png" alt="OCCAM Engenharia" />
+        </section>
+        <img src="/Isometric.png" alt="Homens trabalando com tecnologia" />
+      </main>
+    </Container>
   )
 }
 
