13f3433af29899d44aa80540c903a84d5ea8d4af
Gabriel <gprando55@gmail.com>
2021-01-11 09:56:05 -0300

    feat: add google analytics :zap:

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,18 @@ import { AppProps } from 'next/app'
 import { ThemeProvider } from '@/hooks/theme'
 import GlobalStyle from '../styles/global'
 
+import TagManager from 'react-gtm-module'
+import { useEffect } from 'react'
+
+const tagManagerArgs = {
+  gtmId: 'GTM-T56P86F'
+}
+
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs)
+  }, [])
+
   return (
     <ThemeProvider>
       <Component {...pageProps} />
