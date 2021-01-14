712b13eeed8bc74ca9b9bb989b54c3e574f8ea5e
Gabriel <gprando55@gmail.com>
2021-01-14 12:53:41 -0300

    feat: created page seletive process :sparkles:

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,8 @@ import GlobalStyle from '../styles/global'
 
 import TagManager from 'react-gtm-module'
 import { useEffect } from 'react'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 
 const tagManagerArgs = {
   gtmId: 'GTM-T56P86F'
@@ -16,8 +18,10 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
 
   return (
     <ThemeProvider>
+      <Header />
       <Component {...pageProps} />
       <GlobalStyle />
+      <Footer />
     </ThemeProvider>
   )
 }
