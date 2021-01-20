7235ac9c14dd425ea2f2d674607a7b305326e625
Maria Luiza Medeiros de Freitas <malu_medeirosfreitas@hotmail.com>
2021-01-20 18:14:12 -0300

    - Página de serviços - Modificação no header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import { AppProps } from 'next/app'
 import { ThemeProvider } from '@/hooks/theme'
 import GlobalStyle from '../styles/global'
+import { Header, Footer } from '@/components'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <ThemeProvider>
+      <Header />
       <Component {...pageProps} />
       <GlobalStyle />
+      <Footer />
     </ThemeProvider>
   )
 }
