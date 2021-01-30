3d012d4fd9a75d90e0e85da2c7019ab40c0c1a4e
jefer <jefer@pop-os.localdomain>
2021-01-30 15:53:04 -0300

    🔧 - chore: Create validate of start and end selective process

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,10 +6,30 @@ import { CgMoon, CgSun } from 'react-icons/cg'
 import { Main, HeaderContent, Burguer } from './styles'
 import { GetStaticProps } from 'next'
 import api from '@/services/api'
-import { IProcessSeletive } from '@/pages/processo-seletivo/[slug]'
 interface ISelectiveProcess {
   selectiveProcessActive: boolean
   processSlug: string
+}
+interface IProcessSeletive {
+  marketing_department_description: string
+  interview_start_date: Date
+  start_date: Date
+  slug: string
+  collective_activity_date: Date
+  opening_subscriptions: Date
+  closing_subscriptions: Date
+  end_date: Date
+  finance_department_description: string
+  project_department_description: string
+  legal_department_description: string
+  approved_disclosure_date: Date
+  people_management_department_description: string
+  description: string
+  slogan: string
+  photo_disclosure: {
+    url: string
+  }
+  link_edital: string
 }
 const Header: React.FC = () => {
   const { toggleTheme, theme } = useTheme()
@@ -27,8 +47,8 @@ const Header: React.FC = () => {
       .get<IProcessSeletive[]>('/selective-processes?active=true')
       .then(response => {
         response.data.map(process => {
-          const closingDate = new Date(process.closing_subscriptions)
-          const openingDate = new Date(process.opening_subscriptions)
+          const openingDate = new Date(`${process.start_date} 00:00`)
+          const closingDate = new Date(`${process.end_date} 00:00`)
           if (currentDate <= closingDate && currentDate >= openingDate) {
             setSelectiveProcess({
               selectiveProcessActive: true,
