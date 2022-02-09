1cc2b5bc51ac58be6298fe26b7c544fb3292191d
Daniel Gustavo Favero <daniel.favero@contasimples.com>
2022-02-09 18:13:19 -0300

    chore: adicionado estilos da variante outlined

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -12,6 +12,7 @@ export const Main = styled.button<ButtonStyleProps>`
   width: 100%;
   background: none;
   border: none;
+  outline: none;
   border-radius: 0.8rem;
   text-transform: uppercase;
   padding: 1.6rem 4.8rem;
@@ -70,6 +71,75 @@ export const Main = styled.button<ButtonStyleProps>`
       css`
         background: ${({ theme }) => theme.colors.neutral30};
         color: ${({ theme }) => theme.colors.neutral100};
+        cursor: not-allowed;
+      `}
+    `}
+
+  ${props =>
+    props.variant === 'outlined' &&
+    css`
+      background: none;
+      ${props.color === 'primary' &&
+      css`
+        border: 1px solid ${({ theme }) => theme.colors.primary90};
+        color: ${({ theme }) => theme.colors.primary90};
+
+        &:hover {
+          border: 1px solid ${({ theme }) => theme.colors.primary100};
+          color: ${({ theme }) => theme.colors.primary100};
+          background: rgba(7, 115, 198, 0.15);
+        }
+
+        &:active,
+        &:focus {
+          border: 1px solid ${({ theme }) => theme.colors.primary80};
+          color: ${({ theme }) => theme.colors.primary80};
+          background: rgba(14, 66, 139, 0.2);
+        }
+      `}
+
+      ${props.color === 'secondary' &&
+      css`
+        border: 1px solid ${({ theme }) => theme.colors.neutral100};
+        color: ${({ theme }) => theme.colors.neutral100};
+
+        &:hover {
+          border: 1px solid ${({ theme }) => theme.colors.neutral80};
+          color: ${({ theme }) => theme.colors.neutral80};
+          background: rgba(238, 238, 238, 0.2);
+        }
+
+        &:active,
+        &:focus {
+          border: 1px solid ${({ theme }) => theme.colors.neutral70};
+          color: ${({ theme }) => theme.colors.neutral70};
+          background: rgba(224, 224, 224, 0.4);
+        }
+      `}
+
+      ${props.color === 'warning' &&
+      css`
+        border: 1px solid ${({ theme }) => theme.colors.warning90};
+        color: ${({ theme }) => theme.colors.warning90};
+
+        &:hover {
+          border: 1px solid ${({ theme }) => theme.colors.warning100};
+          color: ${({ theme }) => theme.colors.warning100};
+          background: rgb(228, 91, 91, 0.15);
+        }
+
+        &:active,
+        &:focus {
+          border: 1px solid ${({ theme }) => theme.colors.warning80};
+          color: ${({ theme }) => theme.colors.warning80};
+          background: rgba(186, 51, 51, 0.2);
+        }
+      `}
+
+      ${props.color === 'disabled' &&
+      css`
+        border: 1px solid ${({ theme }) => theme.colors.neutral30};
+        color: ${({ theme }) => theme.colors.neutral30};
         cursor: not-allowed;
       `}
     `}
